5fb36edab439c860bd382054f9e88912055309fc
Denis Seleznev <generalpokrichkin@icloud.com>
2023-02-23 21:36:34 +0300

    add component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<h1>Hello World</h1>
-	</React.StrictMode>
-);
+const App = () => {
+	return <Counter />;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
