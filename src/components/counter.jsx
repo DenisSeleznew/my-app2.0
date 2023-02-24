94836aa799da61b9ca34d5aaac6ce8eb46bc8905
Denis Seleznev <generalpokrichkin@icloud.com>
2023-02-24 16:37:10 +0300

    add conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,22 +17,25 @@ const Counter = () => {
 	const handleDecrement = () => {
 		setCount(prevState => prevState - 1);
 	};
-	const handleTagChange = () => {
-		setTags(["tag4", "tag5"]);
+	const handleTagChange = id => {
+		setTags(prevState => prevState.filter(tag => tag !== id));
 	};
-	return (
-		<>
-			<ul>
-				{tags.map(tag => (
+	const renderTags = () => {
+		return tags.length !== 0
+			? tags.map(tag => (
 					<li
 						key={tag}
 						className="btn btn-primary btn-sm m-2"
-						onClick={handleTagChange}
+						onClick={() => handleTagChange(tag)}
 					>
 						{tag}
 					</li>
-				))}
-			</ul>
+			  ))
+			: "No Tags";
+	};
+	return (
+		<>
+			<ul>{renderTags()}</ul>
 			<span className={getBadgeClasses()}>{formatCount()}</span>
 			<button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
 				+
