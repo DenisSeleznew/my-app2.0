5782b261357fe43342ee04c7bdb760dd199b8685
Denis Seleznev <generalpokrichkin@icloud.com>
2023-02-24 14:26:42 +0300

    add className

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-	const count = 0;
+	const count = -1;
 	const formatCount = () => {
 		return count <= 0 ? "empty" : count;
 	};
+
+	const getBadgeClasses = () => {
+		let classes = "badge m-2 ";
+		return (classes += count <= 0 ? "bg-warning" : "bg-primary");
+	};
+
 	return (
 		<>
-			<span>{formatCount()}</span>
-			<button>+</button>
+			<span className={getBadgeClasses()}>{formatCount()}</span>
+			<button className="btn btn-primary btn-sm m-2">+</button>
 		</>
 	);
 };
