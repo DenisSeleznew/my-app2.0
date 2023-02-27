834ac2159b0c10025f4e49ddb22b016851a27785
Denis Seleznev <generalpokrichkin@icloud.com>
2023-02-28 02:19:00 +0300

    add couners-List

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 const Counter = () => {
 	const [count, setCount] = useState(0);
-	const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
 	const formatCount = () => {
 		return count === 0 ? "empty" : count;
@@ -17,25 +16,9 @@ const Counter = () => {
 	const handleDecrement = () => {
 		setCount(prevState => prevState - 1);
 	};
-	const handleTagChange = id => {
-		setTags(prevState => prevState.filter(tag => tag !== id));
-	};
-	const renderTags = () => {
-		return tags.length !== 0
-			? tags.map(tag => (
-					<li
-						key={tag}
-						className="btn btn-primary btn-sm m-2"
-						onClick={() => handleTagChange(tag)}
-					>
-						{tag}
-					</li>
-			  ))
-			: "No Tags";
-	};
+
 	return (
-		<>
-			<ul>{renderTags()}</ul>
+		<div>
 			<span className={getBadgeClasses()}>{formatCount()}</span>
 			<button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
 				+
@@ -43,7 +26,7 @@ const Counter = () => {
 			<button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
 				-
 			</button>
-		</>
+		</div>
 	);
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
-
-const App = () => {
-	return <Counter />;
-};
+import App from "./app";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
