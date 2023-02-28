bca498f75851cf20f4a5c6840a1bc58f0aff164e
Denis Seleznev <generalpokrichkin@icloud.com>
2023-03-01 01:34:35 +0300

    did my homework

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Counter = props => {
 	const { value } = props;
@@ -9,23 +9,21 @@ const Counter = props => {
 		let classes = "badge m-2 ";
 		return (classes += value === 0 ? "bg-warning" : "bg-primary");
 	};
-	const handleIncrement = () => {
-		console.log("hanle increment");
-		// setValue(prevState => prevState + 1);
-	};
-	const handleDecrement = () => {
-		console.log("hendle decrement");
-		// setValue(prevState => prevState - 1);
-	};
 
 	return (
 		<div>
 			<span>{props.name}</span>
 			<span className={getBadgeClasses()}>{formatValue()}</span>
-			<button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+			<button
+				className="btn btn-primary btn-sm m-2"
+				onClick={() => props.onIncrement(props.id)}
+			>
 				+
 			</button>
-			<button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+			<button
+				className="btn btn-primary btn-sm m-2"
+				onClick={() => props.onDecrement(props.id)}
+			>
 				-
 			</button>
 			<button
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -11,37 +11,45 @@ const CountersList = () => {
 	];
 
 	const [counters, setCounters] = useState(initialState);
+
 	const handleDelete = id => {
 		const newCounters = counters.filter(c => c.id !== id);
 		setCounters(newCounters);
 	};
+
+	const handleIncrement = count => {
+		const newIncrement = counters.filter(
+			count => count.value !== count,
+			(count.value += 1)
+		);
+		setCounters(newIncrement);
+	};
+	const handleDecrement = count => {
+		const newDecrement = counters.filter(
+			item => item.value !== count,
+			(count.value -= 1)
+		);
+		setCounters(newDecrement);
+	};
+
 	const handleReset = () => {
 		setCounters(initialState);
-		console.log("handleReset");
 	};
-	// const handleUpdate = () => {
-	// 	const updatedState = [
-	// 		{ id: 0, value: 1, name: "Ненужная вещь" },
-	// 		{ id: 1, value: 2, name: "Ложка" },
-	// 		{ id: 2, value: 3, name: "Вилка" },
-	// 		{ id: 3, value: 4, name: "Тарелка" },
-	// 		{ id: 4, value: 0, name: "Набор минималиста" },
-	// 	];
-	// 	setCounters(updatedState);
-	// 	console.log("handleUpdate");
-	// };
 
 	return (
 		<>
 			{counters.map(count => (
-				<Counter key={count.id} onDelete={handleDelete} {...count} />
+				<Counter
+					key={count.id}
+					onDelete={handleDelete}
+					onIncrement={() => handleIncrement(count)}
+					onDecrement={() => handleDecrement(count)}
+					{...count}
+				/>
 			))}
 			<button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
 				Сброс
 			</button>
-			{/* <button className="btn btn-primary btn-sm m-2" onClick={handleUpdate}>
-				Обновить состояние
-			</button> */}
 		</>
 	);
 };
